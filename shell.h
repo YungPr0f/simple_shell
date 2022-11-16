3da13e6e7e4d626a90664b4d767502f28f8146e1
Abigail <akinbamireabigail@gmail.com>
2022-11-16 04:59:36 -0800

    simple shell

--- shell.h
+++ shell.h
@@ -1,6 +1,15 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
 
 #endif /* SHELL_H */
